61656316fb5d02679c8b1aae6fbeb2a3bc4afd61
Tom Wright <tom@inflatablecookie.com>
2013-01-09 13:05:07 +0000

    Added package file
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ Package.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * This file is part of the Decode Framework
+ * @license http://opensource.org/licenses/MIT
+ */
+namespace df\apex\packages\touchstoneCore;
+
+use df\core;
+
+class Package extends core\Package {
+    
+    const PRIORITY = 20;
+}
