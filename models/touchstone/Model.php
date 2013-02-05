2ccad966d3b4445600421112b60dae2e4742b323
Tom Wright <tom@inflatablecookie.com>
2013-02-05 23:03:52 +0000

    Added model and label unit
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ models/touchstone/Model.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This file is part of the Decode Framework
+ * @license http://opensource.org/licenses/MIT
+ */
+namespace df\apex\models\touchstone;
+
+use df;
+use df\core;
+use df\axis;
+use df\fire;
+
+class Model extends axis\Model {
+    
+}
