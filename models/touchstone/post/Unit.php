6d72f9ea7a7bc459c6a0811cfbaaf5e936ab8d3d
Tom Wright <tom@inflatablecookie.com>
2013-02-06 12:50:36 +0000

    Added post unit
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ models/touchstone/post/Unit.php
@@ -0,0 +1,16 @@
+<?php 
+/**
+ * This file is part of the Decode Framework
+ * @license http://opensource.org/licenses/MIT
+ */
+namespace df\apex\models\touchstone\post;
+
+use df;
+use df\core;
+use df\apex;
+use df\axis;
+    
+class Unit extends axis\unit\table\Base {
+
+    
+}
