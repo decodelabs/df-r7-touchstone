7558997c90fd40c8edb8fed850e021e625da4bf6
Tom Wright <tom@inflatablecookie.com>
2013-02-06 21:28:53 +0000

    Added Article fire category
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ libraries/fire/category/Article.php
@@ -0,0 +1,16 @@
+<?php 
+/**
+ * This file is part of the Decode Framework
+ * @license http://opensource.org/licenses/MIT
+ */
+namespace df\fire\category;
+
+use df;
+use df\core;
+use df\fire;
+    
+class Article extends Base {
+
+    protected static $_requiredOutputTypes = ['Html'];
+    protected static $_defaultBlocks = ['SimpleTags', 'RawHtml'];
+}
