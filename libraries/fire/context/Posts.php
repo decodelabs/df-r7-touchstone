edbfa981daa083023125a4c20d4eb19ae1b4db68
Tom Wright <tom@inflatablecookie.com>
2014-02-24 14:50:13 +0000

    Added nightfire context
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ libraries/fire/context/Posts.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This file is part of the Decode Framework
+ * @license http://opensource.org/licenses/MIT
+ */
+namespace df\fire\context;
+
+use df;
+use df\core;
+use df\fire;
+
+class Posts extends Base {
+    
+    const DEFAULT_SLUG = 'posts';
+}
